bb9262a0ee0b0f59e6d55ec503155c19a13f4c51
nuclearMonkeys <lyndond@uci.edu>
2019-01-26 15:01:53 -0800

    Preliminary Dialogue Option Added

---
+++ Assets/LyndonScripts/SceneInteract.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneInteract : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void load(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
