63498527226831429d35230c46d203c75ae27724
gavindent <gavindent@hotmail.com>
2019-01-26 10:58:01 -0800

    Changes to StatsManagerController to include set increase and decrease methods

---
+++ Assets/_Scripts/HouseManagerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseManagerController : MonoBehaviour
+{
+    public GameObject statManager;
+
+    private StatsManagerController smc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        smc = statManager.GetComponent<StatsManagerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //list of actions to be done in the house
+    public void RepairHouse()
+    {
+        smc.SetShelterLossRate(smc.shelterHPLoss / 2);
+    }
+}
